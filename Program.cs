d512274d6ffc39cc8be055f68a66a65337707127
ShuhratDoniyev96 <doniyevshuhratgithub@gmail.com>
2022-10-28 21:18:53 +0500

    Initial commit

---
+++ Program.cs
@@ -0,0 +1,16 @@
+﻿using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using System.IO;
+
+namespace SeleniumWebDriver
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            IWebDriver driver = new ChromeDriver();
+            driver.Url = "https://www.google.com/search?q=Hello World";
+            driver.Quit();
+        }
+    }
+}
